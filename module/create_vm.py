55807fbaec31ddefd4db70628a5c5f95c37be1b4
Александр Ю. Тихонов <aleti@m12.onc.local>
2025-09-30 16:20:37 +0500

    update files

---
+++ module/create_vm.py
@@ -0,0 +1,42 @@
+
+def create_vms():
+    from proxmoxer import ProxmoxAPI
+    proxmox = ProxmoxAPI(
+    host= '192.168.100.5',
+    user='root@pam',
+    password='OsI7q3Z',
+    verify_ssl=False
+    )
+            ### Считываем количество нод и их имена
+    nodes=proxmox.nodes.get()
+    node_names = [node['node'] for node in nodes]
+    ### Считывание пользователей из файла users.txt в массив user_list
+    user_list=user_from_file.users() 
+    
+    for i, user in enumerate(user_list):
+        node_claster=node_names[i % len(node_names)]
+        try:
+            proxmox.access.users(user).get()
+            print('Пользователь', user.split('@')[0], 'существует!')
+        except Exception as e:
+            proxmox.access.users.post(
+                userid=user,
+                password='12345678'
+            )
+            print('Пользователь', user.split('@')[0], 'создан!')
+        try:
+            proxmox.pools(user.split('@')[0]).get()
+            print('Пул', user.split('@')[0], 'создан!')
+        except Exception as e:
+            proxmox.pools.post(
+            poolid=user.split('@')[0]
+            )
+            print('Пул', user.split('@')[0], 'существует!')
+        ### клонирование машины altsrv
+        new_vmid = get_free_vmid.get_free_vmid(proxmox, start_from=1000)
+        proxmox.nodes('SRV1-PVE').qemu('100').clone.post(
+            newid=new_vmid,
+            target=node_claster,
+            pool=user.split('@')[0],
+            name='vm1'
+        )
--- script.py
+++ script.py
@@ -1,52 +1,48 @@
 #!/usr/bin/env python3
 import module.user_from_file as user_from_file
 import module.get_free_vmid as get_free_vmid
+import module.create_vm as create_vm
 import os
 
 from proxmoxer import ProxmoxAPI
 proxmox = ProxmoxAPI(
-    host= '192.168.100.5',
-    user='root@pam',
-    password='OsI7q3Z',
-    verify_ssl=False
+host= '192.168.100.5',
+user='root@pam',
+password='OsI7q3Z',
+verify_ssl=False
 )
-    ### Считываем количество нод и их имена
-nodes=proxmox.nodes.get()
-node_names = [node['node'] for node in nodes]
-    ### Считывание пользователей из файла users.txt в массив user_list
-user_list=user_from_file.users() 
+
 
 
 
 def main():
+    menu_options = {
+        1: ("Создать ВМ для пользователей", create_vm.create_vms() ),
+        2: ("Показать список всех ВМ", list_all_vms),
+        3: ("Удалить ВМ по ID", delete_vm),
+        4: ("Выход", exit_program)
+    }
+
+    while True:
+        print("\n" + "="*40)
+        print("       УПРАВЛЕНИЕ ВИРТУАЛЬНЫМИ МАШИНАМИ")
+        print("="*40)
+
+        for key, (desc, func) in menu_options.items():
+            print(f"{key}. {desc}")
+
+        try:
+            choice = int(input("\nВыберите действие: "))
+            if choice in menu_options:
+                action = menu_options[choice][1]  # получаем функцию
+                action()  # вызываем
+                if choice == 4:  # если "выход"
+                    break
+            else:
+                print("❌ Нет такого пункта. Попробуйте снова.")
+        except ValueError:
+            print("❌ Введите число.")
     
-    for i, user in enumerate(user_list):
-        node_claster=node_names[i % len(node_names)]
-        try:
-            proxmox.access.users(user).get()
-            print('Пользователь', user.split('@')[0], 'существует!')
-        except Exception as e:
-            proxmox.access.users.post(
-                userid=user,
-                password='12345678'
-            )
-            print('Пользователь', user.split('@')[0], 'создан!')
-        try:
-            proxmox.pools(user.split('@')[0]).get()
-            print('Пул', user.split('@')[0], 'создан!')
-        except Exception as e:
-            proxmox.pools.post(
-            poolid=user.split('@')[0]
-            )
-            print('Пул', user.split('@')[0], 'существует!')
-        ### клонирование машины altsrv
-        new_vmid = get_free_vmid.get_free_vmid(proxmox, start_from=1000)
-        proxmox.nodes('SRV1-PVE').qemu('100').clone.post(
-            newid=new_vmid,
-            target=node_claster,
-            pool=user.split('@')[0],
-            name='vm1'
-        )
 
 if __name__ == "__main__":
     main()
